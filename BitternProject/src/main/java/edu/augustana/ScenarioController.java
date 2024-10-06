1db88a2c0cfb36cf7ca9218a5d341338c8a86b2f
Leroy <leroygawu-mensah22@augustana.edu>
2024-10-06 10:59:52 -0500

    added a back button for the Scenario controller clas

--- BitternProject/src/main/java/edu/augustana/ScenarioController.java
+++ BitternProject/src/main/java/edu/augustana/ScenarioController.java
@@ -1,4 +1,18 @@
 package edu.augustana;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class ScenarioController {
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private void switchToWelcome(ActionEvent event) throws IOException {
+        RadioApp.setRoot("WelcomeScreen");
+    }
 }
