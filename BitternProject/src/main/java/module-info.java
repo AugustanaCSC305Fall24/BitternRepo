4be274d7c47662b10035a840b7051c28f830659f
Amy <amyzeigler21@augustana.edu>
2024-11-17 12:43:46 -0600

    Added in JSON code to server controller (and seemingly necessary classes?) - don't know how to open in two windows to test

--- BitternProject/src/main/java/module-info.java
+++ BitternProject/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module edu.augustana {
     requires java.desktop;
     requires javafx.media;
     requires org.json;
+    requires com.google.gson;
+    requires tyrus.standalone.client;
 
     opens edu.augustana to javafx.fxml;
     exports edu.augustana;
